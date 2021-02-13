df223939fe11b0bb2caf94dd0c5179db7968a596
Admin <Admin@BEST-KOMP>
2021-02-13 17:11:25 +0300

    Added HW03.Birthday.cproj

---
+++ HomeWorks/HW03.Birthday/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace HW03.Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int birthYear;
+            int birthMonth;
+            int curYear;
+            int curMonth;
+
+            WriteLine("Введите год рождения: ");
+            birthYear = int.Parse(ReadLine());
+
+            WriteLine("Введите месяц рождения: ");
+            birthMonth = int.Parse(ReadLine());
+
+            WriteLine("Введите текущий год: ");
+            curYear = int.Parse(ReadLine());
+
+            WriteLine("Введите текущий месяц: ");
+            curMonth = int.Parse(ReadLine());
+
+            if (curYear >= birthYear)
+            {
+                WriteLine($"Ваш возраст: \nлет: {curYear - birthYear} \nмесяцев: {curMonth - birthMonth}");
+            }
+            ReadLine();
+        }
+    }
+}
