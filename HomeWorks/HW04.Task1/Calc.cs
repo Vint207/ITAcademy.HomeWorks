1caa73a2d16f3a2974788168573b2d281a5f9b9b
Admin <Admin@BEST-KOMP>
2021-02-13 19:37:51 +0300

    Added HW04.Task1.cproj

---
+++ HomeWorks/HW04.Task1/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+
+namespace HW04.Task1
+{
+    public static class Calc
+    {
+        public static void Sum()
+        {
+            double numb1;
+            double numb2;
+
+            WriteLine("Введите первое число:");
+            while (double.TryParse(ReadLine(), out numb1) != true)   //Конвертация строки в число, если это возможно. Иначе повторить ввод  
+            {
+                WriteLine("Введите число:");
+            }
+
+            WriteLine("Введите второе число:");
+            while (double.TryParse(ReadLine(), out numb2) != true)
+            {
+                WriteLine("Введите число:");
+            }
+            WriteLine($"\n{numb1} + {numb2} = {numb1 + numb2}"); 
+        }
+    }
+}
