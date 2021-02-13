35fffcd64545f0865bb7ca3ed679df81a88f92f7
Admin <Admin@BEST-KOMP>
2021-02-13 16:50:27 +0300

    Added HW03.Calculator.Static.cproj

---
+++ HomeWorks/HW03.Calculator.Static/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW03.Calculator.Static
+{
+    class Calculator
+    {
+        /// <summary>
+        /// Returns result a + b
+        /// </summary>
+        /// <returns></returns>
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        /// <summary>
+        /// Returns result a - b
+        /// </summary>
+        /// <returns></returns>
+        public static int Diff(int a, int b)
+        {
+            return a - b;
+        }
+        /// <summary>
+        /// Returns result a * b
+        /// </summary>
+        /// <returns></returns>
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        /// <summary>
+        /// Returns result a / b
+        /// </summary>
+        /// <returns></returns>
+        public static int Quote(int a, int b)
+        {
+            return a / b;
+        }
+        /// <summary>
+        /// Returns result a % b
+        /// </summary>
+        /// <returns></returns>
+        public static double QuoteRest(int a, int b)
+        {
+            return a % b;
+        }
+        /// <summary>
+        ///  Returns result Pi * r * r
+        /// </summary>
+        /// <returns></returns>
+        public static double Square(int r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
