0a305b14915178507cec619fb90d34c3bdb9e4fe
Admin <Admin@BEST-KOMP>
2021-02-13 20:44:19 +0300

    Added HW04.Task5.cproj

---
+++ HomeWorks/HW04.Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace HW04.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90; i > 64; i--)    //От 90 до 65 - десятичные аналоги букв английского алфавита от Z до A в UniCode
+            {
+                WriteLine(ToChar(i));
+            } 
+        }
+    }
+}
