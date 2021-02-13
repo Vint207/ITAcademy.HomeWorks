63f0598b116c718cfdcbcc521b4af8242829cc14
Admin <Admin@BEST-KOMP>
2021-02-13 20:16:15 +0300

    Added HW04.Task3.cproj

---
+++ HomeWorks/HW04.Task3/Calc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace HW04.Task3
+{
+    public static class Calc
+    {
+        public static void Sum()
+        {
+            double numb1;       //Вводимое число 1
+            double numb2;       //Вводимое число 2
+            double buffer;      //Вспомогательная переменная
+
+            WriteLine("Введите первое число:");
+            while (double.TryParse(ReadLine(), out numb1) != true)   //Конвертация строки в число, если это возможно. Иначе повторить ввод  
+            {
+                WriteLine("Введите число:");
+            }
+
+            WriteLine("Введите второе число:");
+            while (double.TryParse(ReadLine(), out numb2) != true)
+            {
+                WriteLine("Введите число:");
+            }
+
+            WriteLine("Введите сумму чисел:");
+            while (double.TryParse(ReadLine(), out buffer) != true ||  buffer != (numb1 + numb2))
+            {
+                WriteLine("Почти угадал...");
+                WriteLine($"Попробуй число больше {numb1 + numb2 - 3} и меньше {numb1 + numb2 + 3}");
+            }
+            WriteLine("Угадал!");
+        }
+    }
+}
